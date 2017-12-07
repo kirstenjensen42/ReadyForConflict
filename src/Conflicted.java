5654cd71816f2905b44a140cc9a7080481c9bb84
Kirsten Jensen <kirstenjensen42@gmail.com>
2017-12-07 15:15:46 -0500

    set up project

---
+++ src/Conflicted.java
@@ -0,0 +1,44 @@
+
+public class Conflicted {
+	
+	private String sadNews;
+	
+	private String reallySadNews;
+	
+	private String pleaseNoMoreReallySadNews;
+	
+	public Conflicted() {
+		sadNews = new String();
+		reallySadNews = new String();
+		pleaseNoMoreReallySadNews = new String();
+	}
+	
+	
+	public String getSadNews() {
+		
+		sadNews = "Something very sad.";
+		
+		return sadNews;
+		
+	}
+	
+	
+	public String getReallySadNews() {
+		
+		reallySadNews = getSadNews() + " You won't believe how sad.";
+		
+		return reallySadNews;
+		
+	}
+	
+	
+	public String getPleaseNoMoreReallySadNews() {
+		
+		pleaseNoMoreReallySadNews = getReallySadNews() + " I won't even tell you it's so sad.";
+		
+		return pleaseNoMoreReallySadNews;
+		
+	}
+	
+	
+}
